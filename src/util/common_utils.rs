5fc8b7d95258490edb54d56197bd618f7767dca0
shikai liu <lsk569937453@gmail.com>
2024-04-25 09:09:01 +0800

    Add the bincode.

--- src/util/common_utils.rs
+++ src/util/common_utils.rs
@@ -1,11 +1,11 @@
-use time::get_time;
+use time::OffsetDateTime;
 
-pub fn ustime() -> i64 {
-    let tv = get_time();
-    tv.sec * 1000000 + (tv.nsec / 1000) as i64
+pub fn ustime() -> i128 {
+    let now = OffsetDateTime::now_utc();
+    now.unix_timestamp_nanos()
 }
 
 /// Current timestamp in milliseconds
-pub fn mstime() -> i64 {
+pub fn mstime() -> i128 {
     ustime() / 1000
 }
