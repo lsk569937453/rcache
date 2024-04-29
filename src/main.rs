5559cae0140201f7ab0554b0671ff69580fac36d
shikai liu <lsk569937453@gmail.com>
2024-04-29 08:46:20 +0800

    Change the logger to tracing.

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,37 @@ use crate::parser::handler::Handler;
 use std::sync::{Arc, Mutex};
 use tokio::net::TcpListener;
 use tokio::task;
+use tracing::metadata::LevelFilter;
+use tracing_appender::non_blocking::{NonBlockingBuilder, WorkerGuard};
+use tracing_appender::rolling;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::Layer;
+use tracing_subscriber::{fmt, layer::SubscriberExt};
 #[macro_use]
-extern crate log;
+extern crate tracing;
 #[macro_use]
 extern crate anyhow;
+fn setup_logger() -> Result<WorkerGuard, anyhow::Error> {
+    let app_file = rolling::daily("./logs", "access.log");
+    let (non_blocking_appender, guard) = NonBlockingBuilder::default()
+        .buffered_lines_limit(10)
+        .finish(app_file);
+    let file_layer = tracing_subscriber::fmt::Layer::new()
+        .with_target(true)
+        .with_ansi(false)
+        .with_writer(non_blocking_appender)
+        .with_filter(tracing_subscriber::filter::LevelFilter::INFO);
 
+    tracing_subscriber::registry()
+        .with(file_layer)
+        .with(tracing_subscriber::filter::LevelFilter::TRACE)
+        .init();
+    Ok(guard)
+}
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    std::env::set_var("RUST_LOG", "info");
-    env_logger::init();
+    let _worker_guard = setup_logger()?;
+
     let addr = "0.0.0.0:6379";
     let listener = TcpListener::bind(&addr)
         .await
